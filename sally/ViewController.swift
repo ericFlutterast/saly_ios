660995f28ec7dd29c4681721d84688316c715e0c
driller <erikGitFlutter@gmail.com>
2025-09-08 19:58:19 +0300

    Initial Commit

---
+++ sally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sally
+//
+//  Created by Эрик Нугзарович on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sallyTests/sallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  sallyTests.swift
+//  sallyTests
+//
+//  Created by Эрик Нугзарович on 9/8/25.
+//
+
+import Testing
+@testable import sally
+
+struct sallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
